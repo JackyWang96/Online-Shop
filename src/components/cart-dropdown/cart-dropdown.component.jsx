fb3aa2d539cb995ecff75c55f8180ac82df1f60f
Jackywang96 <jackywangmel96@gmail.com>
2023-05-27 01:51:15 +1000

    update the checkout component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,27 @@
 import './cart-dropdown.style.scss';
 import Button from "../button/button.component";
+import CartItem from '../cart-item.component/cart-item.component';
+import {useContext} from "react";
+import {CartContext} from "../../contexts/cart.context";
+import {useNavigate} from "react-router-dom";
 
-const CartDropdown = () => (
+const CartDropdown = () => {
+    const {cartItems} = useContext(CartContext);
+    const navigate = useNavigate();
+    const goToCheckoutHandler = () =>{
+        navigate('/checkout');
+        console.log('yes')
+
+    }
+    return(
     <div className='cart-dropdown-container'>
         <div className='cart-item'/>
-        <Button>Go To Check Out</Button>
+        {cartItems.map((item) => (
+            <CartItem key={item.id}cartItem={item}/>))}
+        <Button onClick={goToCheckoutHandler}>Go To Check Out</Button>
     </div>
-
-);
+    )
+};
 
 
 export default CartDropdown;
