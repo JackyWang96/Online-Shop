df7ceab768eb8288242138355e3fd469128e635f
Jackywang96 <jackywangmel96@gmail.com>
2023-06-18 23:54:34 +1000

    fix layout and add new router

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,16 +1,20 @@
 import './category-item.styles.scss';
+import {useNavigate} from "react-router-dom";
 
 const CategoryItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title, route } = category;
+    const navigae = useNavigate();
+    const onNavigateHandler = () => navigae(route);
     return (
         <div className='category-container'>
             <div
+                onClick={onNavigateHandler}
                 className='background-image'
                 style={{
                     backgroundImage: `url(${imageUrl})`,
                 }}
             />
-            <div className='category-body-container'>
+            <div className='category-body-container' onClick={onNavigateHandler}>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
             </div>
